b9bf0ae4ef6b6ecabe3003340ebeacbaeeeb30e8
AlmerDave <almerdavedizon11@gmail.com>
2019-10-21 19:12:43 +0800

    add

--- CustomerDAL.cs
+++ CustomerDAL.cs
@@ -15,12 +15,47 @@ namespace PMS
 
         private static string connString = ConfigurationManager.ConnectionStrings["EmployeeDB"]?.ConnectionString;
 
+        public static List<EmpDetails> searchByCustomer(string id)
+        {
+            List<EmpDetails> search = new List<EmpDetails>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connString))
+                using (SqlCommand command = new SqlCommand("Customer.SearchByCustId", connection))
+                {
+                    connection.Open();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Cust_Id", id);
+
+                    using (SqlDataReader reader =command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            EmpDetails p = new EmpDetails();
+                            p.Cust_FirstName = reader.GetString(0);
+                            p.Cust_LastName = reader.GetString(1);
+                            p.Cust_Gender = reader.GetString(2);
+                            p.Cust_Address = reader.GetString(3);
+                            p.Cust_Contact_No = reader.GetInt32(4);
+                        }
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex, "SQL Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+
+            return search;
+        }
+
         public static void addCustomer (EmpDetails obj)
         {
             try
             {
                 using (SqlConnection connection = new SqlConnection(connString))
-                using (SqlCommand command = new SqlCommand("AddCustomer", connection))
+                using (SqlCommand command = new SqlCommand("Customer.AddCustomer", connection))
                 {
                     connection.Open();
                     command.CommandType = CommandType.StoredProcedure;
